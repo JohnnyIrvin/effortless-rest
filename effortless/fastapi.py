26d3a3ef4bbe91e9ffa91d4bf6113735e4853ea6
Johnathan Irvin <irvinjohnathan@gmail.com>
2023-06-04 00:19:03 -0400

    fix(fastapi): Fixed EffortlessFastAPI import.

--- effortless/fastapi.py
+++ effortless/fastapi.py
@@ -18,11 +18,6 @@
 # LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-from .effortless import Effortless
-from .fastapi import EffortlessFastAPI
 
-__all__ = [
-    "Effortless",
-    "EffortlessFastAPI",
-]
-
+class EffortlessFastAPI:
+    ...
