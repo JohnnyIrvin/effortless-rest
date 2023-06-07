68be35351338fd3a0ae0a58b1cc45131337f1172
Johnathan Irvin <irvinjohnathan@gmail.com>
2023-06-07 19:13:29 -0400

    feat(fastapi): Implemented FastAPI web framework.

--- effortless/fastapi.py
+++ effortless/fastapi.py
@@ -18,6 +18,46 @@
 # LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+from types import ModuleType
+from typing import Dict, TypeVar
+
+from fastapi import FastAPI
+
+from .store import Store
+
+T = TypeVar("T")
+
 
 class EffortlessFastAPI:
-    ...
+    def __init__(self) -> None:
+        self._app = FastAPI()        
+
+    def create_routes(self, models: ModuleType, stores: Dict[T, Store[T]]):
+        for model, store in stores.items():
+            name = model.__name__.lower()
+            
+            @self._app.get(f"/{name}")
+            def get_all():
+                return store.all()
+            
+            @self._app.get(f"/{name}/{{uuid}}")
+            def get(uuid: str):
+                return store.get(uuid)
+            
+            @self._app.post(f"/{name}")
+            def create(m: model):
+                created_uuid = store.create(m)
+
+                print(created_uuid)
+
+                return {
+                    "uuid": created_uuid
+                }
+            
+            @self._app.delete(f"/{name}/{{uuid}}")
+            def delete(uuid: str):
+                store.delete(uuid)
+            
+            @self._app.put(f"/{name}/{{uuid}}")
+            def update(uuid: str, m: model):
+                store.update(uuid, m)
